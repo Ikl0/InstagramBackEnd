66e0d109aed95c8ced0de5793a456f8e60ecf4d7
Ikl0 <klimovs2008van@gmail.com>
2022-11-10 18:30:22 +0300

    add hashing password with bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -3,40 +3,45 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
 
-router.get('/',(req,res)=>{
+const bcrypt = require('bcryptjs')
+
+
+router.get('/', (req, res) => {
     res.send("Hello")
 })
 
-router.post('/signup',(req,res)=>{
-    const {name,email,password} = req.body
+router.post('/signup', (req, res) => {
+    const { name, email, password } = req.body
 
-    if(!email || !password || !name)
-    {
-       return res.status(422).json({error:"Fill all fields."})
+    if (!email || !password || !name) {
+        return res.status(422).json({ error: "Fill all fields." })
     }
-User.findOne({email:email})
-.then((savedUser)=>{
-    if(savedUser){
-        return res.status(422).json({error:"User with this email is already exists."})
-    }
+    User.findOne({ email: email })
+        .then((savedUser) => {
+            if (savedUser) {
+                return res.status(422).json({ error: "User with this email is already exists." })
+            }
 
-    const user = new User({
-        email,
-        password,
-        name
-    })
+            bcrypt.hash(password, 12)
+                .then(hashedpassword => {
+                    const user = new User({
+                        email,
+                        password:hashedpassword,
+                        name
+                    })
 
-    user.save()
-    .then(user=>{
-        res.json({message:"saved successfully"})
-    })
-    .catch(error=>{
-        console.log(error)
-    })
-})
-.catch(error=>{
-    console.log(error)
-})
+                    user.save()
+                        .then(user => {
+                            res.json({ message: "saved successfully" })
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        })
+                })
+        })
+        .catch(error => {
+            console.log(error)
+        })
 })
 
 module.exports = router
