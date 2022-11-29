38ad7c99b4caaea2907434800685cac1881bb417
Ikl0 <klimovs2008van@gmail.com>
2022-11-29 18:23:19 +0300

    add routes for likes and comments

--- routes/post.js
+++ routes/post.js
@@ -45,4 +45,52 @@ router.get('/mypost', requireLogin, (req, res)=>{
     })
 })
 
+router.put('/like', requireLogin, (req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((error,result)=>{
+        if(error){
+            return res.status(422).json({error:error})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.put('/dislike', requireLogin, (req,res)=>{
+    Post.findByIdAndUpdate(req.body.postId,{
+        $pull:{likes:req.user._id}
+    },{
+        new:true
+    }).exec((error,result)=>{
+        if(error){
+            return res.status(422).json({error:error})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
+router.put('/comment', requireLogin, (req,res)=>{
+    const comment = {
+        text:req.body.text,
+        postedBy:req.user._id
+    }
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{likcommentses:comment}
+    },{
+        new:true
+    })
+    .populate("comments.postedBy","_id name")
+    .exec((error,result)=>{
+        if(error){
+            return res.status(422).json({error:error})
+        }else{
+            res.json(result)
+        }
+    })
+})
+
 module.exports = router
