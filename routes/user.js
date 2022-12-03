5dc5e0705e9bf53373780935c73497e5662c7342
Ikl0 <klimovs2008van@gmail.com>
2022-12-03 14:44:47 +0300

    add profile page of another user

---
+++ routes/user.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const requireLogin = require('../middleware/requirelogin')
+const Post = mongoose.model("Post")
+const User = mongoose.model("USer")
+
+router.get('/user/:id', (req, res) => {
+    User.findOne({ _id: req.params.id })
+        .select("-password")
+        .then(user => {
+            Post.find({ postedBy: req.params.id })
+                .populate("postedBy", "_id name")
+                .exec((error, posts) => {
+                    if (error) {
+                        return res.status(422).json({ error: error })
+                    }
+                    res.json({ user, posts })
+                })
+        }).catch(error => {
+            return res.status(404).json({ error: "User not found" })
+        })
+})
+
+
+module.exports = router
